fa1b0823484765685615a46bc7ed7b654bcefee3
Jayson Cuadro <JaysonPineC@iqmetrix.com>
2017-01-16 17:28:37 +0800

    Changed database connection to local

--- BeerTapsAPI/BeerTaps.API.Data/BeerTapsApiDataModel.cs
+++ BeerTapsAPI/BeerTaps.API.Data/BeerTapsApiDataModel.cs
@@ -1,6 +1,8 @@
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.SqlServer.Server;
 
 namespace BeerTapsAPI.Data
 {
@@ -14,17 +16,31 @@ namespace BeerTapsAPI.Data
         public BeerTapsApiDataModel()
             : base("name=BeerTapsApiDataModel")
         {
+            InitializeConnectionStringParameters();
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<BeerTapsApiDataModel, Migrations.Configuration>());
         }
 
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            
+                
         }
 
         public DbSet<Office> OfficesData { get; set; }
         public DbSet<Tap> TapsData { get; set; }
-       
+
+        private void InitializeConnectionStringParameters()
+        {
+            //string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            const string BASE_PROJECT = "BeerTapsAPI.WebApi";
+            const string DATA_PROJECT = "BeerTaps.API.Data";
+
+            string path = AppDomain.CurrentDomain.BaseDirectory.Replace(BASE_PROJECT, DATA_PROJECT);
+            path = string.Concat(path, "database\\");
+
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+            
+        }
+
     }
 }
